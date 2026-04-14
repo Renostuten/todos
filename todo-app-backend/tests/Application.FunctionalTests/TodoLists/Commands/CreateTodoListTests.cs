a80720f130a554946495b44d8006758ef9317f90
Brendan He <brendan.he@dash.com.au>
2026-04-14 11:47:46 +1000

    added more tests for creating lists

--- todo-app-backend/tests/Application.FunctionalTests/TodoLists/Commands/CreateTodoListTests.cs
+++ todo-app-backend/tests/Application.FunctionalTests/TodoLists/Commands/CreateTodoListTests.cs
@@ -48,4 +48,61 @@ public class CreateTodoListTests : TestBase
         list.CreatedBy.ShouldBe(userId);
         list.Created.ShouldBe(DateTime.Now, TimeSpan.FromMilliseconds(10000));
     }
+
+    [Test]
+    public async Task ShouldCreateMultipleListsForSameUser()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var command1 = new CreateTodoListCommand
+        {
+            Title = "Work"
+        };
+
+        var command2 = new CreateTodoListCommand
+        {
+            Title = "Personal"
+        };
+
+        var id1 = await TestApp.SendAsync(command1);
+        var id2 = await TestApp.SendAsync(command2);
+
+        var list1 = await TestApp.FindAsync<TodoList>(id1);
+        var list2 = await TestApp.FindAsync<TodoList>(id2);
+
+        list1.ShouldNotBeNull();
+        list1!.Title.ShouldBe(command1.Title);
+        list1.CreatedBy.ShouldBe(userId);
+
+        list2.ShouldNotBeNull();
+        list2!.Title.ShouldBe(command2.Title);
+        list2.CreatedBy.ShouldBe(userId);
+    }
+
+    [Test]
+    public async Task ShouldAllowSameTitleForDifferentUsers()
+    {
+        var userId1 = await TestApp.RunAsUserAsync("user1", "Password1!", Array.Empty<string>());
+        
+        await TestApp.SendAsync(new CreateTodoListCommand
+        {
+            Title = "Shopping"
+        });
+
+        var userId2 = await TestApp.RunAsUserAsync("user2", "Password2!", Array.Empty<string>());
+
+        var command = new CreateTodoListCommand
+        {
+            Title = "Shopping"
+        };
+
+        var id = await TestApp.SendAsync(command);
+
+        var list = await TestApp.FindAsync<TodoList>(id);
+
+        list.ShouldNotBeNull();
+        list!.Title.ShouldBe(command.Title);
+        list.CreatedBy.ShouldBe(userId2);
+    }
 }
+
