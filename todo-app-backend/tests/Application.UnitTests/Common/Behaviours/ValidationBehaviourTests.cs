c59dae454891aec6fb7ddbcbf0f3618f9ce5c736
Ren <142564582+Renostuten@users.noreply.github.com>
2026-04-14 14:33:09 +1000

    Add unit tests for validation pipeline behaviour

---
+++ todo-app-backend/tests/Application.UnitTests/Common/Behaviours/ValidationBehaviourTests.cs
@@ -0,0 +1,74 @@
+﻿using FluentValidation;
+using MediatR;
+using NUnit.Framework;
+using Shouldly;
+using todo_app_backend.Application.Common.Behaviours;
+using todo_app_backend.Application.Common.Exceptions;
+
+namespace todo_app_backend.Application.UnitTests.Common.Behaviours;
+
+public class ValidationBehaviourTests
+{
+    [Test]
+    public async Task ShouldInvokeNextWhenNoValidatorsAreRegistered()
+    {
+        var behaviour = new ValidationBehaviour<SampleRequest, Unit>(Array.Empty<IValidator<SampleRequest>>());
+        var nextWasCalled = false;
+
+        var response = await behaviour.Handle(
+            new SampleRequest { Name = string.Empty },
+            () =>
+            {
+                nextWasCalled = true;
+                return Task.FromResult(Unit.Value);
+            },
+            CancellationToken.None);
+
+        nextWasCalled.ShouldBeTrue();
+        response.ShouldBe(Unit.Value);
+    }
+
+    [Test]
+    public async Task ShouldInvokeNextWhenValidationPasses()
+    {
+        var validators = new[] { new SampleRequestValidator() };
+        var behaviour = new ValidationBehaviour<SampleRequest, Unit>(validators);
+        var nextWasCalled = false;
+
+        await behaviour.Handle(
+            new SampleRequest { Name = "valid" },
+            () =>
+            {
+                nextWasCalled = true;
+                return Task.FromResult(Unit.Value);
+            },
+            CancellationToken.None);
+
+        nextWasCalled.ShouldBeTrue();
+    }
+
+    [Test]
+    public void ShouldThrowValidationExceptionWhenValidationFails()
+    {
+        var validators = new[] { new SampleRequestValidator() };
+        var behaviour = new ValidationBehaviour<SampleRequest, Unit>(validators);
+
+        Should.ThrowAsync<ValidationException>(() => behaviour.Handle(
+            new SampleRequest { Name = string.Empty },
+            () => Task.FromResult(Unit.Value),
+            CancellationToken.None));
+    }
+
+    private sealed class SampleRequest
+    {
+        public string Name { get; init; } = string.Empty;
+    }
+
+    private sealed class SampleRequestValidator : AbstractValidator<SampleRequest>
+    {
+        public SampleRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
