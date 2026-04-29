e3077e20835850958d8fb850dc444a31c8300488
Brendan He <brendan.he@dash.com.au>
2026-04-29 11:47:20 +1000

    setup frontend for component testing

--- todo-app-frontend/vite.config.ts
+++ todo-app-frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import babel from "@rolldown/plugin-babel";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
+  },
 });
