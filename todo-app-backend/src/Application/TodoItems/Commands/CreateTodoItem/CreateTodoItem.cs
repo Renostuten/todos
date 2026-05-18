17ab502a7296e2872444b245b93e4be606b1f293
Brendan He <brendan.he@dash.com.au>
2026-05-18 16:44:31 +1000

    updated create todo item to check for valid list id

--- todo-app-backend/src/Application/TodoItems/Commands/CreateTodoItem/CreateTodoItem.cs
+++ todo-app-backend/src/Application/TodoItems/Commands/CreateTodoItem/CreateTodoItem.cs
@@ -13,14 +13,26 @@ public record CreateTodoItemCommand : IRequest<int>
 public class CreateTodoItemCommandHandler : IRequestHandler<CreateTodoItemCommand, int>
 {
     private readonly IApplicationDbContext _context;
+    private readonly IUser _user;
 
-    public CreateTodoItemCommandHandler(IApplicationDbContext context)
+    public CreateTodoItemCommandHandler(IApplicationDbContext context, IUser user)
     {
         _context = context;
+        _user = user;
     }
 
     public async Task<int> Handle(CreateTodoItemCommand request, CancellationToken cancellationToken)
     {
+        var list = await _context.TodoLists
+            .FirstOrDefaultAsync(
+                l => l.Id == request.ListId && l.CreatedBy == _user.Id,
+                cancellationToken);
+
+        if (list is null)
+        {
+            throw new NotFoundException(nameof(TodoList), request.ListId.ToString());
+        }
+        
         var entity = new TodoItem
         {
             ListId = request.ListId,
