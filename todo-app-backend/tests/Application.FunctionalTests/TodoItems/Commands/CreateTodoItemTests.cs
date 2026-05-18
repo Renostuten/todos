5f0494d7c9d4691212b7b4021ad57b3168fa23d8
Brendan He <brendan.he@dash.com.au>
2026-05-18 16:45:26 +1000

    added create todo item tests for coverage

--- todo-app-backend/tests/Application.FunctionalTests/TodoItems/Commands/CreateTodoItemTests.cs
+++ todo-app-backend/tests/Application.FunctionalTests/TodoItems/Commands/CreateTodoItemTests.cs
@@ -1,4 +1,5 @@
-﻿using todo_app_backend.Application.Common.Exceptions;
+﻿using Humanizer;
+using todo_app_backend.Application.Common.Exceptions;
 using todo_app_backend.Application.TodoItems.Commands.CreateTodoItem;
 using todo_app_backend.Application.TodoLists.Commands.CreateTodoList;
 using todo_app_backend.Domain.Entities;
@@ -20,10 +21,12 @@ public class CreateTodoItemTests : TestBase
     {
         var userId = await TestApp.RunAsDefaultUserAsync();
 
-        var listId = await TestApp.SendAsync(new CreateTodoListCommand
+        var list = new TodoList
         {
             Title = "New List"
-        });
+        };
+        await TestApp.AddAsync(list);
+        var listId = list.Id;
 
         var command = new CreateTodoItemCommand
         {
@@ -42,5 +45,70 @@ public class CreateTodoItemTests : TestBase
         item.Created.ShouldBe(DateTime.Now, TimeSpan.FromMilliseconds(10000));
         item.LastModifiedBy.ShouldBe(userId);
         item.LastModified.ShouldBe(DateTime.Now, TimeSpan.FromMilliseconds(10000));
+    }
+
+    [Test]
+    public async Task ShouldRequireExistingList()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var command = new CreateTodoItemCommand
+        {
+            ListId = 999,
+            Title = "Tasks"
+        };
+
+        await Should.ThrowAsync<NotFoundException>(() => TestApp.SendAsync(command));
+    }
+
+    [Test]
+    public async Task ShouldRequireListOwnedByUser()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list1 = new TodoList
+        {
+            Title = "New List1"
+        };
+        await TestApp.AddAsync(list1);
+        var list1Id = list1.Id;
+
+        var otherUserId = await TestApp.RunAsUserAsync("other@local", "Testing1234!", []);
+        
+        var list2 = new TodoList
+        {
+            Title = "New List2"
+        };
+        await TestApp.AddAsync(list2);
+        var list2Id = list2.Id;
+
+        var command = new CreateTodoItemCommand
+        {
+            ListId = list1Id,
+            Title = "Tasks"
+        };
+
+        await Should.ThrowAsync<NotFoundException>(() => TestApp.SendAsync(command));
+    }
+
+    [Test]
+    public async Task ShouldRequireTitle()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list = new TodoList
+        {
+            Title = "New List"
+        };
+        await TestApp.AddAsync(list);
+        var listId = list.Id;
+
+        var command = new CreateTodoItemCommand
+        {
+            ListId = listId,
+            Title = null
+        };
+
+        await Should.ThrowAsync<ValidationException>(() => TestApp.SendAsync(command));
     }
 }
