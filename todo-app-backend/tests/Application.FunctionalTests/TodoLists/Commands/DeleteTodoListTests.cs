929cf9d20d2ff1fad9ca825ad0d7f6c1ab645cbb
Brendan He <brendan.he@dash.com.au>
2026-05-19 11:53:22 +1000

    added more tests for coverage into delete todo list tests

--- todo-app-backend/tests/Application.FunctionalTests/TodoLists/Commands/DeleteTodoListTests.cs
+++ todo-app-backend/tests/Application.FunctionalTests/TodoLists/Commands/DeleteTodoListTests.cs
@@ -16,15 +16,69 @@ public class DeleteTodoListTests : TestBase
     [Test]
     public async Task ShouldDeleteTodoList()
     {
-        var listId = await TestApp.SendAsync(new CreateTodoListCommand
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list = new TodoList
         {
             Title = "New List"
-        });
+        };
+        await TestApp.AddAsync(list);
+        var listId = list.Id;
 
         await TestApp.SendAsync(new DeleteTodoListCommand(listId));
 
-        var list = await TestApp.FindAsync<TodoList>(listId);
+        list = await TestApp.FindAsync<TodoList>(listId);
 
         list.ShouldBeNull();
+    }
+
+    [Test]
+    public async Task ShouldDeleteTodoItemsWhenDeletingTodoList()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list = new TodoList
+        {
+            Title = "New List",
+            CreatedBy = userId
+        };
+        await TestApp.AddAsync(list);
+
+        var item = new TodoItem
+        {
+            ListId = list.Id,
+            Title = "Task",
+            CreatedBy = userId
+        };
+        await TestApp.AddAsync(item);
+
+        await TestApp.SendAsync(new DeleteTodoListCommand(list.Id));
+
+        var deletedList = await TestApp.FindAsync<TodoList>(list.Id);
+        var deletedItem = await TestApp.FindAsync<TodoItem>(item.Id);
+
+        deletedList.ShouldBeNull();
+        deletedItem.ShouldBeNull();
+    }
+
+    [Test]
+    public async Task ShouldNotDeleteTodoListOfAnotherUser()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list = new TodoList
+        {
+            Title = "New List",
+            CreatedBy = userId
+        };
+        await TestApp.AddAsync(list);
+
+        var otheruserId = await TestApp.RunAsUserAsync("other@local", "Testing1234!", []);
+
+        var command = new DeleteTodoListCommand(list.Id);
+
+        var action = () => TestApp.SendAsync(command);
+
+        await Should.ThrowAsync<NotFoundException>(action);
     }
 }
