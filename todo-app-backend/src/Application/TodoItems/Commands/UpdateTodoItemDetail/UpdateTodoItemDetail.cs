ad5dd3fe19dad724bd51f3807707946df2370dc2
Brendan He <brendan.he@dash.com.au>
2026-05-19 10:26:44 +1000

    updated update todo item details command to check owner ship and destination list exists

--- todo-app-backend/src/Application/TodoItems/Commands/UpdateTodoItemDetail/UpdateTodoItemDetail.cs
+++ todo-app-backend/src/Application/TodoItems/Commands/UpdateTodoItemDetail/UpdateTodoItemDetail.cs
@@ -1,4 +1,5 @@
 ﻿using todo_app_backend.Application.Common.Interfaces;
+using todo_app_backend.Domain.Entities;
 using todo_app_backend.Domain.Enums;
 
 namespace todo_app_backend.Application.TodoItems.Commands.UpdateTodoItemDetail;
@@ -17,18 +18,36 @@ public record UpdateTodoItemDetailCommand : IRequest
 public class UpdateTodoItemDetailCommandHandler : IRequestHandler<UpdateTodoItemDetailCommand>
 {
     private readonly IApplicationDbContext _context;
+    private readonly IUser _user;
 
-    public UpdateTodoItemDetailCommandHandler(IApplicationDbContext context)
+    public UpdateTodoItemDetailCommandHandler(IApplicationDbContext context, IUser user)
     {
         _context = context;
+        _user = user;
     }
 
     public async Task Handle(UpdateTodoItemDetailCommand request, CancellationToken cancellationToken)
     {
         var entity = await _context.TodoItems
-            .FindAsync([request.Id], cancellationToken);
+            .Include(i => i.List)
+            .FirstOrDefaultAsync(
+                i => i.Id == request.Id && i.List.CreatedBy == _user.Id,
+                cancellationToken);
 
-        Guard.Against.NotFound(request.Id, entity);
+        if (entity is null)
+        {
+            throw new NotFoundException(nameof(TodoItem), request.Id.ToString());
+        }
+
+        var destinationListExists = await _context.TodoLists
+            .AnyAsync(
+                l => l.Id == request.ListId && l.CreatedBy == _user.Id,
+                cancellationToken);
+
+        if (!destinationListExists)
+        {
+            throw new NotFoundException(nameof(TodoList), request.ListId.ToString());
+        }
 
         entity.ListId = request.ListId;
         entity.Priority = request.Priority;
