8fdd78346e478faa52823983d5da384e6eab2557
Brendan He <brendan.he@dash.com.au>
2026-05-19 10:27:08 +1000

    added more tests for coverage in update todo item details tests

--- todo-app-backend/tests/Application.FunctionalTests/TodoItems/Commands/UpdateTodoItemDetailTests.cs
+++ todo-app-backend/tests/Application.FunctionalTests/TodoItems/Commands/UpdateTodoItemDetailTests.cs
@@ -1,5 +1,4 @@
 ﻿using todo_app_backend.Application.TodoItems.Commands.CreateTodoItem;
-using todo_app_backend.Application.TodoItems.Commands.UpdateTodoItem;
 using todo_app_backend.Application.TodoItems.Commands.UpdateTodoItemDetail;
 using todo_app_backend.Application.TodoLists.Commands.CreateTodoList;
 using todo_app_backend.Domain.Entities;
@@ -12,7 +11,7 @@ public class UpdateTodoItemDetailTests : TestBase
     [Test]
     public async Task ShouldRequireValidTodoItemId()
     {
-        var command = new UpdateTodoItemCommand { Id = 99, Title = "New Title" };
+        var command = new UpdateTodoItemDetailCommand { Id = 99, ListId = 1, Priority = PriorityLevel.Low };
 
         await Should.ThrowAsync<NotFoundException>(() => TestApp.SendAsync(command));
     }
@@ -22,16 +21,20 @@ public class UpdateTodoItemDetailTests : TestBase
     {
         var userId = await TestApp.RunAsDefaultUserAsync();
 
-        var listId = await TestApp.SendAsync(new CreateTodoListCommand
+        var list = new TodoList
         {
             Title = "New List"
-        });
+        };
+        await TestApp.AddAsync(list);
+        var listId = list.Id;
 
-        var itemId = await TestApp.SendAsync(new CreateTodoItemCommand
+        var item = new TodoItem
         {
             ListId = listId,
             Title = "New Item"
-        });
+        };
+        await TestApp.AddAsync(item);
+        var itemId = item.Id;
 
         var command = new UpdateTodoItemDetailCommand
         {
@@ -43,7 +46,7 @@ public class UpdateTodoItemDetailTests : TestBase
 
         await TestApp.SendAsync(command);
 
-        var item = await TestApp.FindAsync<TodoItem>(itemId);
+        item = await TestApp.FindAsync<TodoItem>(itemId);
 
         item.ShouldNotBeNull();
         item!.ListId.ShouldBe(command.ListId);
@@ -52,5 +55,152 @@ public class UpdateTodoItemDetailTests : TestBase
         item.LastModifiedBy.ShouldNotBeNull();
         item.LastModifiedBy.ShouldBe(userId);
         item.LastModified.ShouldBe(DateTime.Now, TimeSpan.FromMilliseconds(10000));
+    }
+
+    [Test]
+    public async Task ShouldNotUpdateTodoItemOfAnotherUser()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list = new TodoList
+        {
+            Title = "New List"
+        };
+        await TestApp.AddAsync(list);
+        var listId = list.Id;
+
+        var item = new TodoItem
+        {
+            ListId = listId,
+            Title = "New Item"
+        };
+        await TestApp.AddAsync(item);
+        var itemId = item.Id;
+
+        var otheruserId = await TestApp.RunAsUserAsync("other@local", "Testing1234!", []);
+
+        var command = new UpdateTodoItemDetailCommand
+        {
+            Id = itemId,
+            ListId = listId,
+            Note = "This is the note.",
+            Priority = PriorityLevel.High
+        };
+
+        await Should.ThrowAsync<NotFoundException>(() => TestApp.SendAsync(command));
+    }
+
+    [Test]
+    public async Task ShouldRequireValidDestinationListId()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list = new TodoList
+        {
+            Title = "New List"
+        };
+        await TestApp.AddAsync(list);
+        var listId = list.Id;
+
+        var item = new TodoItem
+        {
+            ListId = listId,
+            Title = "New Item"
+        };
+        await TestApp.AddAsync(item);
+        var itemId = item.Id;
+
+        var command = new UpdateTodoItemDetailCommand
+        {
+            Id = itemId,
+            ListId = 99,
+            Note = "This is the note.",
+            Priority = PriorityLevel.High
+        };
+
+        await Should.ThrowAsync<NotFoundException>(() => TestApp.SendAsync(command));
+    }
+
+    [Test]
+    public async Task ShouldMoveTodoItemToAnotherList()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list1 = new TodoList
+        {
+            Title = "New List 1"
+        };
+        await TestApp.AddAsync(list1);
+        var list1Id = list1.Id;
+
+        var list2 = new TodoList
+        {
+            Title = "New List 2"
+        };
+        await TestApp.AddAsync(list2);
+        var list2Id = list2.Id;
+
+        var item = new TodoItem
+        {
+            ListId = list1Id,
+            Title = "New Item"
+        };
+        await TestApp.AddAsync(item);
+        var itemId = item.Id;
+
+        var command = new UpdateTodoItemDetailCommand
+        {
+            Id = itemId,
+            ListId = list2Id,
+            Note = "This is the note.",
+            Priority = PriorityLevel.High
+        };
+
+        await TestApp.SendAsync(command);
+
+        item = await TestApp.FindAsync<TodoItem>(itemId);
+
+        item.ShouldNotBeNull();
+        item!.ListId.ShouldBe(command.ListId);
+    }
+
+    [Test]
+    public async Task ShouldNotMoveTodoItemToAnotherUsersList()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list1 = new TodoList
+        {
+            Title = "New List 1"
+        };
+        await TestApp.AddAsync(list1);
+        var list1Id = list1.Id;
+
+        var otheruserId = await TestApp.RunAsUserAsync("other@local", "Testing1234!", []);
+
+        var list2 = new TodoList
+        {
+            Title = "New List 2"
+        };
+        await TestApp.AddAsync(list2);
+        var list2Id = list2.Id;
+
+        var item = new TodoItem
+        {
+            ListId = list2Id,
+            Title = "New Item"
+        };
+        await TestApp.AddAsync(item);
+        var itemId = item.Id;
+
+        var command = new UpdateTodoItemDetailCommand
+        {
+            Id = itemId,
+            ListId = list1Id,
+            Note = "This is the note.",
+            Priority = PriorityLevel.High
+        };
+
+        await Should.ThrowAsync<NotFoundException>(() => TestApp.SendAsync(command));
     }
 }
