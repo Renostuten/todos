74feddccc3f651f4c622a930feb0e3ce4ef6b994
Brendan He <brendan.he@dash.com.au>
2026-05-19 10:46:25 +1000

    added more tests for coverage in toggle todo item tests

--- todo-app-backend/tests/Application.FunctionalTests/TodoItems/Commands/ToggleTodoItemTests.cs
+++ todo-app-backend/tests/Application.FunctionalTests/TodoItems/Commands/ToggleTodoItemTests.cs
@@ -21,18 +21,20 @@ public class ToggleTodoItemTests : TestBase
     {
         var userId = await TestApp.RunAsDefaultUserAsync();
 
-        var listId = await TestApp.SendAsync(new CreateTodoListCommand
+        var list = new TodoList
         {
             Title = "New List"
-        });
+        };
+        await TestApp.AddAsync(list);
+        var listId = list.Id;
 
-        var createCommand = new CreateTodoItemCommand
+        var item = new TodoItem
         {
             ListId = listId,
-            Title = "Tasks"
+            Title = "New Item"
         };
-
-        var itemId = await TestApp.SendAsync(createCommand);
+        await TestApp.AddAsync(item);
+        var itemId = item.Id;
 
         var toggleCommand = new ToggleTodoItemCommand
         {
@@ -41,11 +43,97 @@ public class ToggleTodoItemTests : TestBase
         };
 
         await TestApp.SendAsync(toggleCommand);
-        var item = await TestApp.FindAsync<TodoItem>(itemId);
+        item = await TestApp.FindAsync<TodoItem>(itemId);
 
         item.ShouldNotBeNull();
         item.Done.ShouldBe(true);
         item.LastModifiedBy.ShouldBe(userId);
         item.LastModified.ShouldBe(DateTime.Now, TimeSpan.FromMilliseconds(10000));
+    }
+
+    [Test]
+    public async Task ShouldNotToggleTodoItemOfAnotherUser()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list = new TodoList
+        {
+            Title = "New List"
+        };
+        await TestApp.AddAsync(list);
+        var listId = list.Id;
+
+        var item = new TodoItem
+        {
+            ListId = listId,
+            Title = "New Item"
+        };
+        await TestApp.AddAsync(item);
+        var itemId = item.Id;
+
+        var otheruserId = await TestApp.RunAsUserAsync("other@local", "Testing1234!", []);
+
+        var toggleCommand = new ToggleTodoItemCommand
+        {
+            Id = itemId,
+            Done = true
+        };
+
+        await Should.ThrowAsync<NotFoundException>(() => TestApp.SendAsync(toggleCommand));
+    }
+
+    [Test]
+    public async Task ShouldRequireValidItemId()
+    {
+        var toggleCommand = new ToggleTodoItemCommand
+        {
+            Id = 999,
+            Done = true
+        };
+
+        await Should.ThrowAsync<NotFoundException>(() => TestApp.SendAsync(toggleCommand));
+    }
+
+    [Test]
+    public async Task ShouldToggleTwice()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var list = new TodoList
+        {
+            Title = "New List"
+        };
+        await TestApp.AddAsync(list);
+        var listId = list.Id;
+
+        var item = new TodoItem
+        {
+            ListId = listId,
+            Title = "New Item"
+        };
+        await TestApp.AddAsync(item);
+        var itemId = item.Id;
+
+        var toggleCommand1 = new ToggleTodoItemCommand
+        {
+            Id = itemId,
+            Done = true
+        };
+
+        await TestApp.SendAsync(toggleCommand1);
+        item = await TestApp.FindAsync<TodoItem>(itemId);
+        item.ShouldNotBeNull();
+        item.Done.ShouldBe(true);
+
+        var toggleCommand2 = new ToggleTodoItemCommand
+        {
+            Id = itemId,
+            Done = false
+        };
+
+        await TestApp.SendAsync(toggleCommand2);
+        item = await TestApp.FindAsync<TodoItem>(itemId);
+        item.ShouldNotBeNull();
+        item.Done.ShouldBe(false);
     }
 }
