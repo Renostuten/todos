9938f721eb38f4c5b96ac2b3683f0c3e1c2f7936
Brendan He <brendan.he@dash.com.au>
2026-05-21 10:23:27 +1000

    updated setup to clear all renders after each test

--- todo-app-frontend/src/tests/setup.ts
+++ todo-app-frontend/src/tests/setup.ts
@@ -1,6 +1,8 @@
 import { afterEach, vi } from "vitest";
 import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
 
 afterEach(() => {
+  cleanup();
   vi.clearAllMocks();
 });
